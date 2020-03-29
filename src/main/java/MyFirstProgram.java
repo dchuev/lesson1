c3281a5b7b9b14841d0f3f736f0119cffa6a9a6e
Denis Chuev <fear.kefir@gmail.com>
2020-03-29 13:12:32 +0300

    Добавлены классы и функции с первого занятия, добавлен фреймворк testng

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь Прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
+}
