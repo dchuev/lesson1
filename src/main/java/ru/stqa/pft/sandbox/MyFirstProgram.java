89f2ab18f8651ab0c60fbc7cb192259efae7b800
Denis Chuev <fear.kefir@gmail.com>
2020-03-30 22:44:15 +0300

    ifthenelse

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,5 +8,11 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4, 6);
         System.out.println("Площадь Прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        double x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+
+        System.out.println(Point.distance(p1, p2));
     }
 }
